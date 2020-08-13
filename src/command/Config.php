8d5480a5806689b946840f7a312c4089cdd97224
topextend <57352110+topextend@users.noreply.github.com>
2020-08-13 14:46:55 +0800

    Update Config.php

--- src/command/Config.php
+++ src/command/Config.php
@@ -18,6 +18,8 @@ namespace think\admin\command;
 
 use think\admin\Command;
 use think\console\Input;
+use think\console\input\Argument;
+use think\console\input\Option;
 use think\console\Output;
 
 class Config extends Command
